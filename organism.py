0e861522c72f4ec3a342e82e9e4ccd26bbab8841
Kacperlsky <nothingisimpossiblechannel@gmail.com>
2020-10-27 19:22:14 +0100

    plot all the organisms in the same time

--- organism.py
+++ organism.py
@@ -13,6 +13,12 @@ class organism:
         self.yposition = yposition
         self.energy = energy
 
+    def update(xposition, yposition, xdelta, ydelta):
+        self.xposition = xposition
+        self.yposition = yposition
+        self.xdelta = xdelta
+        self.ydelta = ydelta
+
 class food:
     def __init__(self, xposition, yposition):
         self.xposition = xposition
@@ -36,29 +42,36 @@ def initsim(sizex, sizey, organismnum, foodnum):
         foods.append(fd)
         i = i + 1
 
-def plotlo():
+def plotlo(sizex, sizey):
     fig, ax = plt.subplots()
-    fig.set_size_inches(9.6, 5.4)
-    plt.xlim(0, 100)
-    plt.ylim(0, 100)
+    plt.xlim(0, sizex)
+    plt.ylim(0, sizey)
     plt.title("Test")
+    for org in organisms:
+        orgcircle = Circle([org.xposition,org.yposition], 2, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
+        ax.add_artist(orgcircle)
+        pass
+    """
     circle = Circle([20,20], 2, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
     circle2 = Circle([80,80], 2, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
     circle3 = Circle([50,50], 25, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
+    
     ax.add_artist(circle)
     ax.add_artist(circle2)
     ax.add_artist(circle3)
     ax.set_aspect('equal')
-    frame = plt.gca()
-    
+    """
     plt.show()
 
 
 
 #### 
+def run(sizex, sizey, organismnum, foodnum):
+    initsim(sizex, sizey, organismnum, foodnum)
+    plotlo(sizex, sizey)
 ###run 
-initsim(100, 200, 2, 3)
-plotlo()
 
+
+run(200, 200, 10, 10)
 print("ORGANISMS position x, position y, energy",[(o.xposition, o.yposition, o.energy) for o in organisms])
 print("FOOD position x, position y",[(f.xposition, f.yposition) for f in foods])
