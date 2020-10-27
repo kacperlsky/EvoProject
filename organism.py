177554830ef982c8133954fb858cd6289da54074
Kacperlsky <nothingisimpossiblechannel@gmail.com>
2020-10-27 21:02:14 +0100

    managed to move the organisms and save the images

--- organism.py
+++ organism.py
@@ -12,12 +12,13 @@ class organism:
         self.xposition = xposition
         self.yposition = yposition
         self.energy = energy
-
-    def update(xposition, yposition, xdelta, ydelta):
+    """
+    def updateorganism(xposition, yposition, xdelta, ydelta):
         self.xposition = xposition
         self.yposition = yposition
         self.xdelta = xdelta
         self.ydelta = ydelta
+    """
 
 class food:
     def __init__(self, xposition, yposition):
@@ -42,15 +43,23 @@ def initsim(sizex, sizey, organismnum, foodnum):
         foods.append(fd)
         i = i + 1
 
-def plotlo(sizex, sizey):
+def updateorg(organisms, xdelta, ydelta):
+    for org in organisms:
+        org.xposition = org.xposition + xdelta
+        org.yposition = org.yposition + ydelta
+
+def plotlo(sizex, sizey, i):
     fig, ax = plt.subplots()
     plt.xlim(0, sizex)
     plt.ylim(0, sizey)
     plt.title("Test")
     for org in organisms:
-        orgcircle = Circle([org.xposition,org.yposition], 2, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
+        orgcircle = Circle([org.xposition,org.yposition], 4, facecolor = 'darkgreen', zorder=10)
         ax.add_artist(orgcircle)
+        orgedge = Circle([org.xposition,org.yposition], 8, facecolor='lightgreen', zorder=8)
+        ax.add_artist(orgedge)
         pass
+    ax.set_aspect('equal')
     """
     circle = Circle([20,20], 2, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
     circle2 = Circle([80,80], 2, edgecolor = 'darkslateblue', facecolor = 'mediumslateblue', zorder=5)
@@ -59,19 +68,22 @@ def plotlo(sizex, sizey):
     ax.add_artist(circle)
     ax.add_artist(circle2)
     ax.add_artist(circle3)
-    ax.set_aspect('equal')
+    
     """
+    plt.savefig('img/'+str(i)+'.png', dpi=100)
     plt.show()
 
 
 
 #### 
 def run(sizex, sizey, organismnum, foodnum):
-    initsim(sizex, sizey, organismnum, foodnum)
-    plotlo(sizex, sizey)
+    for i in range(10):
+        initsim(sizex, sizey, organismnum, foodnum)
+        plotlo(sizex, sizey, i)
+        updateorg(organisms, 10, 5)
 ###run 
 
 
-run(200, 200, 10, 10)
+run(1000, 1000, 10, 10)
 print("ORGANISMS position x, position y, energy",[(o.xposition, o.yposition, o.energy) for o in organisms])
 print("FOOD position x, position y",[(f.xposition, f.yposition) for f in foods])
